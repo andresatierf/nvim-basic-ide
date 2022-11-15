a2e5016a93dd42fcf70c871c3333d9e45f123d2a
Andre Freitas <andrecfreitas58@gmail.com>
2022-11-15 18:06:55 +0000

    fix: one dark configuration

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,1 @@
-local colorscheme = "onedark"
-
--- color variant for one dark
-vim.g.onedark_config = {
-	style = "warmer",
-}
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	return
-end
+require("user.onedark")
